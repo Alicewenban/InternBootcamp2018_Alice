a74142b3fd30a5327ea5955ac9a769641194db8d
Alice Wenban <Alice.Wenban@softwire.com>
2018-06-26 14:13:58 +0100

    up to 13

--- FizzBuzz/index.js
+++ FizzBuzz/index.js
@@ -4,18 +4,48 @@ function multThree(a){
 function multFive(a){
     return(a%5 === 0);
 }
+function multSeven(a){
+    return(a%7 === 0);
+}
+function multEleven(a){
+    return(a%11 === 0);
+}
+function multThirteen(a){
+    return(a%13 === 0);
+}
 
-for (i = 1; i <=100; i++) { 
+for (i = 1; i <=200; i++) { 
+    var ans = '';
+    var notNum= true;
     if(multThree(i)){
-        if(multFive(i)){
-            console.log('FizzBuzz'); 
+        ans = 'Fizz';
+        notNum=false;
+    }
+    if(multFive(i)){
+        ans=ans.concat('Buzz');
+        notNum=false;
+    }
+    if(multSeven(i)){
+        ans=ans.concat('Bang');
+        notNum=false;
+    }
+
+    if(multEleven(i)){
+        ans='Bong';
+        notNum=false;
+    }
+    if(multThirteen(i)){
+        if(multThree(i)){
+            ans=[ans.slice(0, 4), 'Fezz', ans.slice(4)].join('');         
         }else{
-            console.log('Fizz'); 
+            ans='Fezz'.concat(ans);
         }
-    }else if(multFive(i)){
-        console.log('Buzz');
-    }else{
+        notNum=false;
+    }
+    if(notNum){
         console.log(i);    
+    }else{
+        console.log(ans)
     }
     
 }
