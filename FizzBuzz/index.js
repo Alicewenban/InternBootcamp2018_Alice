a7249681e25178af4b47bfc18b7ea946d656e713
Alice Wenban <Alice.Wenban@softwire.com>
2018-06-26 17:05:53 +0100

    made everything fucntiony step 1

--- FizzBuzz/index.js
+++ FizzBuzz/index.js
@@ -3,39 +3,66 @@ const readline = require('readline-sync');
 function divisertest(a,b){
     return(a%b === 0);
 }
-var ruleApply= function(valid){ return function(ruleNum){ return function(testNum){return valid && divisertest(testNum,ruleNum)}}}
+function findBpos(ans){
+    for(c=0; c<ans.length;c=c+4){
+        if(ans[c]==='B'){
+            return(c);
+        }  
+    }
+    return ans.length;
+}
+var ruleFun= function(valid){ return function(ruleNum){ return function(testNum){return valid && divisertest(testNum,ruleNum)}}}
 
 console.log("How many numbers do you want?")
 const num = parseInt(readline.prompt());
+var ruleapply=Array(num);
+var ruleFunctions=Array(num);
+//rules inputting
+for (let a=0; a<num; a++){
+    ruleapply[a]=function(a){return a};
+}   
+// adding pre set rules
+ruleFunctions[3]= function(ans){ return 'Fizz'.concat(ans);}
+ruleFunctions[5]= function(ans){ return ans.concat('Buzz');}
+ruleFunctions[7]= function(ans){ return ans.concat('Bang');}
+ruleFunctions[11]= function(ans){ return "Bong";}
+ruleFunctions[13]= function(ans){ 
+    const n = findBpos(ans);
+    return [ans.slice(0,n),"Fezz",ans.slice(n)].join('');
+}
+ruleFunctions[17]= function(ans){
+    for(q=0; q<(ans.length-4); q=q+4){
+     ans=ans.slice(4).concat(ans.slice(0,4));   
+    }
+    return ans;
+}
 
 console.log("what rules do you want? \n\ please select from the following 3,5,7,11,13,17 \n\ seprate by commers e.g if you want 3 5 and 7 input 3,5,7 ")
-const rules = readline.prompt();
-const arrRules = rules.split(',');
-var rule3= function(num){return false};
-var rule5= function(num){return false};
-var rule7= function(num){return false};
-var rule11= function(num){return false};
-var rule13= function(num){return false};
-var rule17= function(num){return false};
+const rule = readline.prompt();
+const arrRules = rule.split(',');
+for (a=0; a<18; a++){
+    ruleapply[a]=function(num){return false};
+}   
+
 for(p=0; p<arrRules.length; p++){
     switch(arrRules[p]){
         case('3'):
-            var rule3 = ruleApply(true)(3);
+            ruleapply[3] = ruleFun(true)(3);
         break;
         case('5'):
-            var rule5 = ruleApply(true)(5);
+            ruleapply[5]  = ruleFun(true)(5);
         break;
         case('7'):
-            var rule7 = ruleApply(true)(7);
+            ruleapply[7]  = ruleFun(true)(7);
         break;
         case('11'):
-            var rule11 = ruleApply(true)(11);
+            ruleapply[11]  = ruleFun(true)(11);
         break;
         case('13'):
-            var rule13 = ruleApply(true)(13);
+            ruleapply[13]  = ruleFun(true)(13);
         break;
         case('17'):
-            var rule17 = ruleApply(true)(17);
+            ruleapply[17]  = ruleFun(true)(17);
         break;
     }
 }
@@ -44,30 +71,29 @@ for(p=0; p<arrRules.length; p++){
 for (i = 1; i <=num; i++) { 
     var ans = '';
     var notNum= true;
-    if(rule3(i)){
-        ans = 'Fizz';
+    //add specil rule for 11
+    if(ruleapply[3](i)){
+        ans = ruleFunctions[3](ans);
         notNum=false;
     }
-    if(rule13(i)){
-        ans=ans.concat('Fezz');
+    if(ruleapply[13](i)){
+        ans = ruleFunctions[13](ans);
         notNum=false;
     }
-    if(rule5(i)){
-        ans=ans.concat('Buzz');
+    if(ruleapply[5](i)){
+        ans = ruleFunctions[5](ans);
         notNum=false;
     }
-    if(rule7(i)){
-        ans=ans.concat('Bang');
+    if(ruleapply[7](i)){
+        ans = ruleFunctions[7](ans);
         notNum=false;
     }
-    if(rule11(i)){
-        ans='Bong';
+    if(ruleapply[11](i)){
+        ans = ruleFunctions[11](ans);
         notNum=false;
     }
-    if(rule17(i)){
-        for(q=0; q<(ans.length-4); q=q+4){
-            ans=ans.slice(4).concat(ans.slice(0,4));
-        }
+    if(ruleapply[17](i)){
+        ans = ruleFunctions[17](ans);
     }
 
 
