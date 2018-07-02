93ab72d8cd240c633508ce0b42b4e8b179b662a4
Alice Wenban <Alice.Wenban@softwire.com>
2018-07-02 12:25:51 +0100

    added map

--- BusBoard/frontend/bike.js
+++ BusBoard/frontend/bike.js
@@ -1,3 +1,14 @@
+  var bikes;
+  function loadMap() {
+    if (document.querySelectorAll('#map').length > 0)
+    {
+      var js_file = document.createElement('script');
+      js_file.type = 'text/javascript';
+      js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCNQJf9rOC5mKTnRM8xF2oVdizKOK0IwGs&callback=initMap&language=en';
+      document.getElementsByTagName('head')[0].appendChild(js_file);
+    }
+  };
+
 function submit(){
     console.log('submit press');
 
@@ -10,8 +21,8 @@ function submit(){
     
     xhttp.onload = function() {
         // Handle response here using e.g. xhttp.status, xhttp.response, xhttp.responseText
-
-        var bikes = JSON.parse(xhttp.responseText);
+        
+        bikes = JSON.parse(xhttp.responseText);
         var list = document.getElementById("bikeList");
         while(list.firstChild) {
             list.removeChild(list.firstChild);
@@ -23,8 +34,40 @@ function submit(){
             li.appendChild(document.createTextNode(niceBikeString));
             list.appendChild(li);
         });
-        console.log(bikes)
+       
+        console.log(bikes) 
+        loadMap();
     }
     
     xhttp.send();
 }
+var map;
+function initMap() {
+    //console.log('initmap is running')
+    map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 8, center: {lat: 51.5074, lng: 0.1278}});
+    plotMarkers();
+  
+  }
+
+  function plotMarkers(){
+    markers = [];
+    bounds = new google.maps.LatLngBounds();
+
+    bikes.forEach(function (marker) {
+      var position = new google.maps.LatLng(marker.lat, marker.long);
+  
+      markers.push(
+        new google.maps.Marker({
+          position: position,
+          map: map,
+          animation: google.maps.Animation.DROP
+        })
+      );
+  
+      bounds.extend(position);
+    });
+  
+    map.fitBounds(bounds);
+  }
+
